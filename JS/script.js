94070aa0d993c11e9c40328c49ba17e8e06d09ad
Artem <Grimdvl@gmail.com>
2021-05-02 13:33:41 +0200

    2

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,19 @@
-console.log(1);
+const numberOfFilms = +prompt('Сколько фильмов вы уже видели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцните его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцните его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
