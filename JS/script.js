b6598be7cdac666543f9105d6c7a1982bf6c2f6b
Artem <grimdvl@gmail.com>
2021-12-18 15:34:23 +0200

    1stcommitpractic

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,6 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже видели?', '');
+"use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -9,9 +11,9 @@ const personalMovieDB = {
 };
 
 const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцните его?', ''),
+      b = +prompt('На сколько оцените его?', ''),
       c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцните его?', '');
+      d = +prompt('На сколько оцените его?', '');
 
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
